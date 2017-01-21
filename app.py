3b32e8a5dabb3b7354b33e3bd97d19d31025afdc
ReshefElisha <reshef.elisha@gmail.com>
2017-01-21 18:42:30 -0500

    half done, progress

--- app.py
+++ app.py
@@ -13,6 +13,21 @@ def home():
 	online_users = list(mongo.db.users.find({'online':True}))
 	#return jsonify([d for d in online_users])
 	return json.dumps(online_users, default=json_util.default)
+	
+@app.route('/patron/<patid>')
+def get_patron_json(patid):
+	json.dumps(list(mongo.db.patrons.find({'id':patid})))
+	
+@app.route('/createpatron/<patid>')
+def create_patron(patid):
+	mongo.db.patrons.insert({'id':patid})
+	return json.dumps(list(mongo.db.patrons.find({'id':patid})), default=json_util.default)
+	
+@app.route('/addprop/<patid>/environment/<envname>')
+def add_env(patid, envname):
+	patron = list(mongo.db.patrons.find({'id':patid}))[0]
+	patron.
+	
 
 if __name__ == "__main__":
 	app.run()
